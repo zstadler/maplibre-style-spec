687a853915f92e1686194c9880c5a70ea63072ec
zstadler <zeev.stadler@gmail.com>
2024-10-19 14:08:00 +0000

    Deploying to gh-pages from @ zstadler/maplibre-style-spec@4e5589a80717ff46d74166b3be5b2798dd4333e9 🚀

--- assets/index-3061118e.js
+++
@@ -1,8 +0,0 @@
-import{g as h,a as i,i as r,c as o,t as u}from"./entry-client-3342931a.js";import{M as c}from"./markdown-58a8f836.js";import{s}from"./v8-3eae868e.js";import{I as y}from"./items-594533a9.js";import"./property-816f77eb.js";import"./subtitle-cfde4caa.js";const f=u("<div><!#><!/><!#><!/>");function v(){const l=`# Light
-    
-A style's \`light\` property provides a global light source for that style. Since this property is the light used to light extruded features, you will only see visible changes to your map style when modifying this property if you are using extrusions.
-
-\`\`\`json
-"light": ${JSON.stringify(s.$root.light.example,null,2)}
-\`\`\`
-`;return(()=>{const t=h(f),n=t.firstChild,[e,a]=i(n.nextSibling),g=e.nextSibling,[p,m]=i(g.nextSibling);return r(t,o(c,{content:l}),e,a),r(t,o(y,{headingLevel:"3",get entry(){return s.light}}),p,m),t})()}export{v as default};
--- assets/index-e230934d.js
+++
@@ -1,9 +0,0 @@
-import{g as $,a as r,i as n,c as o,t as g}from"./entry-client-3342931a.js";import{M as d}from"./markdown-58a8f836.js";import{s as i}from"./v8-3eae868e.js";import{I as f}from"./items-594533a9.js";import"./property-816f77eb.js";import"./subtitle-cfde4caa.js";const _=g("<div><!#><!/><!#><!/>");function N(){const a=`# Terrain
-
-Add 3D terrain to the map.
-
-\`\`\`json
-"terrain": ${JSON.stringify(i.$root.terrain.example,null,2)}
-\`\`\`
-
-`;return(()=>{const t=$(_),s=t.firstChild,[e,m]=r(s.nextSibling),l=e.nextSibling,[p,c]=r(l.nextSibling);return n(t,o(d,{content:a}),e,m),n(t,o(f,{headingLevel:"3",get entry(){return i.terrain}}),p,c),t})()}export{N as default};
